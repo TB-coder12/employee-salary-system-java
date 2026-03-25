2048ca4e1030afddc3acbc63d587cf623bc83e98
TAMOGHNA BASU <mathbasu9@gmail.com>
2026-03-26 01:14:28 +0530

    Remove unnecessary line breaks in Trainee class

--- Trainee.java
+++ Trainee.java
@@ -1,6 +1,4 @@
 class Trainee extends Employee {
-
-    // Constructor
     Trainee(long id, String name, String address, long phone, double salary) {
         super(id, name, address, phone);
         this.basicSalary = salary;
