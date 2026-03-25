d793c11a99517e0d00fa31a6dbaf0fb2ef85d790
TAMOGHNA BASU <mathbasu9@gmail.com>
2026-03-26 01:11:41 +0530

    Refactor Employee class for better structure

--- Employee.java
+++ Employee.java
@@ -6,16 +6,12 @@ class Employee {
     double basicSalary;
     double specialAllowance = 250.80;
     double hra = 1000.50;
-
-    // Constructor
     Employee(long id, String name, String address, long phone) {
         this.employeeId = id;
         this.employeeName = name;
         this.employeeAddress = address;
         this.employeePhone = phone;
     }
-
-    // Salary Calculation
     void calculateSalary() {
         double salary = basicSalary
                 + (basicSalary * specialAllowance / 100)
@@ -23,8 +19,6 @@ class Employee {
 
         System.out.println("Salary of " + employeeName + " = " + salary);
     }
-
-    // Transport Allowance (10%)
     void calculateTransportAllowance() {
         double transportAllowance = (10 * basicSalary) / 100;
         System.out.println("Transport Allowance of " + employeeName + " = " + transportAllowance);
