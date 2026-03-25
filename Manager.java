9f31504b11e4476b3b5d7e2a6b7cf04d46675578
TAMOGHNA BASU <mathbasu9@gmail.com>
2026-03-26 01:13:16 +0530

    Update Manager.java

--- Manager.java
+++ Manager.java
@@ -1,12 +1,8 @@
 class Manager extends Employee {
-
-    // Constructor
     Manager(long id, String name, String address, long phone, double salary) {
         super(id, name, address, phone);
         this.basicSalary = salary;
     }
-
-    // Override transport allowance (15%)
     @Override
     void calculateTransportAllowance() {
         double transportAllowance = (15 * basicSalary) / 100;
