36a1123572d7934ee83013e0dcec77a75aec17b2
TAMOGHNA BASU <mathbasu9@gmail.com>
2026-03-26 01:12:36 +0530

    Remove unnecessary line breaks in InheritanceActivity

--- InheritanceActivity.java
+++ InheritanceActivity.java
@@ -1,14 +1,9 @@
 public class InheritanceActivity {
     public static void main(String[] args) {
-
-        // Test Case 1: Manager
         Manager manager = new Manager(126534, "Peter", "Chennai India", 237844, 65000);
         manager.calculateSalary();
         manager.calculateTransportAllowance();
-
         System.out.println();
-
-        // Test Case 2: Trainee
         Trainee trainee = new Trainee(29846, "Jack", "Mumbai India", 442085, 45000);
         trainee.calculateSalary();
         trainee.calculateTransportAllowance();
